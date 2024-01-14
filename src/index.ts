f4438f4cc6ab59ea09d310f6a9b62d3cafee7dd8
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-01-14 23:03:43 +0530

    added environment variables and dotenv to support it

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import 'dotenv/config'
 
 const app = express();
+const PORT = process.env.PORT || 5500;
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello")
 });
 
-app.listen(3000, () => {
-    console.log("Server running on 3000");
-    console.log("Open: http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+    console.log(`Open: http://localhost:${PORT}`);
 })
