6d7c45dbee03c87861f0ec162d9a55cd8f06a6b8
prathu9 <prathameshpgs@gmail.com>
2024-01-23 12:30:41 +0530

    initialized typeorm appdatasource

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import apiRoute from "./routes/api";
 import 'dotenv/config'
+import { AppDataSource } from './datasource';
 
 const app = express();
 app.use(express.json());
@@ -9,7 +10,13 @@ const PORT = process.env.PORT || 5500;
 
 app.use("/", apiRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-    console.log(`Open: http://localhost:${PORT}`);
+AppDataSource.initialize()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+        console.log(`Open: http://localhost:${PORT}`);
+    })
+})
+.catch(err => {
+    console.log(err);
 })
